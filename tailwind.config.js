41c5d03b4dfa18e1faf50fb14bfeea9143d766ce
ykuruvilla <yasha.kuruvilla@gmail.com>
2023-05-22 23:42:31 +0100

    build projects section

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: { sans: ["Poppins", "system-ui", "sans-serif"] },
-      colors: { purple: "#8444df", lightgrey: "#5f6f81", black: "#333333" },
+      colors: { green: "#44ba23", lightgrey: "#5f6f81", black: "#333333" },
     },
   },
   plugins: [],
