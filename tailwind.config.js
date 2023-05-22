11eb988a4b86a732432fafbc6fb5e5f36de1cd6a
ykuruvilla <yasha.kuruvilla@gmail.com>
2023-05-22 17:41:49 +0100

    add hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: { sans: ["Poppins", "system-ui", "sans-serif"] },
+      colors: { purple: "#8444df", lightgrey: "#5f6f81", black: "#333333" },
+    },
   },
   plugins: [],
 };
